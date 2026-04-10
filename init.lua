b6204d8dd28752139edd5cc18706bccb7c500e15
andrii <lukanukandruha@gmail.com>
2026-04-10 14:00:22 +0300

    feat: add rust language server

--- init.lua
+++ init.lua
@@ -608,7 +608,7 @@ require('lazy').setup({
         clangd = {},
         -- gopls = {},
         pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
