63d4e85eb6e6c0c92e64517de40f0e26e55d1af6
andrii <lukanukandruha@gmail.com>
2026-05-03 18:55:24 +0300

    feat: add jdtls-language-server

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -61,19 +61,10 @@ return { -- LSP Plugins
     ---@type table<string, vim.lsp.Config>
     local servers = {
       clangd = {},
-      -- gopls = {},
       pyright = {},
       rust_analyzer = {},
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`ts_ls`) will work just fine
-      -- ts_ls = {},
-
+      jdtls = {},
       stylua = {},
-
-      -- Special Lua Config, as recommended by neovim help docs
       lua_ls = {
         on_init = function(client)
           if client.workspace_folders then
